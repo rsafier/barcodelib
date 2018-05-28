04acd2230895d8270ba2a635801fbea7e0beab5c
zHaytam <haytam.zanid@gmail.com>
2018-05-28 22:08:04 +0000

    .NET Standard support

---
+++ Standard/Standard.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using BarcodeLib;
+
+namespace Standard.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Barcode b = new Barcode();
+            Image img = b.Encode(TYPE.UPCA, "038000356216", Color.Black, Color.White, 290, 120);
+            img.Save("example.png", ImageFormat.Png);
+        }
+    }
+}
